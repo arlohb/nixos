95d5a46bdd62f967899161dac42a9ede8e5b2cdf
arlohb <arlo@urwick.co.uk>
2023-03-29 22:07:19 +0100

    Now using NUR flake instead of tarball

--- conf/core.nix
+++ conf/core.nix
@@ -13,16 +13,4 @@
 
   nixpkgs.config.allowUnfree = true;
   hardware.enableAllFirmware = true;
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball {
-      url =
-        "https://github.com/nix-community/NUR/archive/02fe4ead56489bd62b2967c51e0f1dda2ff6066f.tar.gz";
-      # Found with `nix-prefetch-url --unpack {above url}`
-      sha256 = "1vpwilcpcnh4h8j0y8p5h2g46xw5fy9iywl9rg8xr21imwh7ys8h";
-    }) {
-      nurpkgs = pkgs;
-      inherit pkgs;
-    };
-  };
 }
--- flake.nix
+++ flake.nix
@@ -1,16 +1,33 @@
 rec {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    nur.url = "github:nix-community/NUR";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     hyprland.url = "github:hyprwm/Hyprland";
   };
-  outputs = { self, nixpkgs, home-manager, hyprland }: {
+
+  outputs = { self, nixpkgs, nur, home-manager, hyprland }: rec {
+    system = "x86_64-linux";
+
     nixosConfigurations.arlo-laptop2 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
+
       modules = [
+        {
+          nixpkgs.config.packageOverrides = pkgs: {
+            nur = import nur {
+              inherit pkgs;
+              nurpkgs = import nixpkgs { inherit system; };
+            };
+          };
+        }
+
         hyprland.nixosModules.default
         {
           programs.hyprland = {
@@ -19,7 +36,9 @@ rec {
             xwayland.hidpi = false;
           };
         }
+
         ./configuration.nix
+
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
