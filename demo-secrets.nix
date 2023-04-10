f8f8f1ca017c9417ba02dfd046bfed606cdac82a
arlohb <arlo@urwick.co.uk>
2023-04-10 16:08:17 +0100

    Updated demo secrets

--- demo-secrets.nix
+++ demo-secrets.nix
@@ -2,4 +2,12 @@
   # Generated with:
   # nix run nixpkgs#mkpasswd -- -m SHA-512 -s
   initialHashedPassword = "$id$salt$hash";
+
+  # Git user info
+  git = {
+    userName = "username";
+    userEmail = "email";
+  };
+  # Copied to ~/.config/git/credentials
+  git-credentials = "https://user:password@host.com";
 }
