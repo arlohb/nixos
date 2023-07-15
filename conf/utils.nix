57e227c35ea63f78035ee47217bc225064652929
arlohb <arlo@urwick.co.uk>
2023-07-15 16:21:10 +0100

    Added docs for utils.nix

--- conf/utils.nix
+++ conf/utils.nix
@@ -1,4 +1,12 @@
 rec {
+  # Lots of very common options are very long,
+  # So I've remapped them to shorter names:
+  #   - environment.systemPackages                           -> pkgs
+  #   - environment.persistence."/nix/persistent"            -> persist
+  #   - environment.persistence."/nix/persistent".users.arlo -> userPersist
+  #   - home-manager.users.arlo                              -> hm
+  #
+  # The other reason for this function it to provide special inputs to the modules.
   betterModule =
     # Inputs not part of usual nixos modules
     # This includes the system hostname
@@ -35,6 +43,13 @@ rec {
       home-manager.users.arlo = hm;
     };
 
+  # This runs the betterModule function on a list of module paths.
+  # The purporse of this is the add in the moduleInputs.
+  #
+  # This is harder than it should be, as I have to destructor them,
+  # even if I don't use them.
+  # I believe this is so NixOS knows which module inputs to provide to the module,
+  # as even in a completely lazy language, all the module inputs may be too much?
   loadBetterModules = specialInputs: map (
     path:
     { pkgs, config, lib, ... }@moduleInputs:
