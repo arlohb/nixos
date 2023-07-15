c8cd52e69e09ebc341a5ce35929d23fd9448f5c7
arlohb <arlo@urwick.co.uk>
2023-07-15 16:31:52 +0100

    Turned off nvim spider skip insignificant punctuation

--- conf/neovim/neovim.nix
+++ conf/neovim/neovim.nix
@@ -58,6 +58,10 @@ in
         {
           plugin = nvim-spider;
           config = ''
+            require("spider").setup {
+              skipInsignificantPunctuation = false,
+            }
+
             vim.keymap.set({"n", "o", "x"}, "w", function() require("spider").motion("w") end, { desc = "Spider-w" })
             vim.keymap.set({"n", "o", "x"}, "e", function() require("spider").motion("e") end, { desc = "Spider-e" })
             vim.keymap.set({"n", "o", "x"}, "b", function() require("spider").motion("b") end, { desc = "Spider-b" })
