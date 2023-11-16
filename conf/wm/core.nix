4ea623e298330ccbbaf4b53e5a0c3f69d3fa8eed
arlohb <arlo@urwick.co.uk>
2023-11-16 14:35:03 +0000

    Update

--- conf/wm/core.nix
+++ conf/wm/core.nix
@@ -18,7 +18,7 @@
   # This is all the stuff I wouldn't need if I used a desktop environment
   pkgs = with pkgs; [
     # For setting backgrounds
-    nur.repos.aleksana.swww
+    swww
     # The top bar and more
     eww-wayland
     # Control media with media keys
