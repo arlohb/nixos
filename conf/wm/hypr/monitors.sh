ea38ec344d59c01ec4b6b72f89c1eefd265daf7b
arlohb <arlo@urwick.co.uk>
2023-12-27 11:19:08 +0000

    Changed pc monitor names for new gpu

--- conf/wm/hypr/monitors.sh
+++ conf/wm/hypr/monitors.sh
@@ -10,9 +10,9 @@ swww init
 sleep 0.1
 
 if [ "$(cat /etc/hostname)" == "arlo-nix" ]; then
-    swww img -o DVI-D-2 /etc/nixos/backgrounds/disco_elysium/cryptid.jpg
-    swww img -o HDMI-A-1 /etc/nixos/backgrounds/disco_elysium/title_screen.jpeg
-    swww img -o DVI-D-1 /etc/nixos/backgrounds/disco_elysium/whirling_in_rags.jpg
+    swww img -o DP-3 /etc/nixos/backgrounds/disco_elysium/cryptid.jpg
+    # swww img -o HDMI-A-1 /etc/nixos/backgrounds/disco_elysium/title_screen.jpeg
+    swww img -o DP-2 /etc/nixos/backgrounds/disco_elysium/whirling_in_rags.jpg
 else
     swww img -o eDP-1 /etc/nixos/backgrounds/rainbow-l.jpg
 fi
