d2baea9fad73289010184e055e6d068a3c42bac8
arlohb <arlo@urwick.co.uk>
2024-02-07 13:57:18 +0000

    Added nvim column ruler

--- conf/neovim/init.lua
+++ conf/neovim/init.lua
@@ -53,6 +53,9 @@ opt.shiftwidth = 4 -- num:  Size of an indent
 opt.softtabstop = 4 -- num:  Number of spaces tabs count for in insert mode
 opt.tabstop = 4 -- num:  Number of spaces tabs count for
 
+-- Line at 80 char
+opt.colorcolumn = "80"
+
 -- Splits
 opt.splitright = true -- bool: Place new window to right of current one
 opt.splitbelow = true -- bool: Place new window below the current one
