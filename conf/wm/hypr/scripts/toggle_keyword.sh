077e1d253d25dc4e981f5f3886feb88817cb1f09
arlohb <arlo@urwick.co.uk>
2024-03-08 14:58:49 +0000

    Fixed toggle_keyword with new hyprland version

--- conf/wm/hypr/scripts/toggle_keyword.sh
+++ conf/wm/hypr/scripts/toggle_keyword.sh
@@ -2,6 +2,11 @@
 
 set initial $(hyprctl getoption $argv | awk 'NR==2{print $2}')
 
-set new $(math 1 - $initial)
+# False and true are commands in fish
+# Returning statuses 1 and 0 respectively
+# Effectively inverting a bool
+$initial
+
+set new $status
 
 hyprctl keyword $argv $new
