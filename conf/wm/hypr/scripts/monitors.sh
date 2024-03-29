68fd63bb153577590ad574f4b61305c12c5ec189
arlohb <arlo@urwick.co.uk>
2024-03-29 19:12:10 +0000

    Switched to home wallpapers

--- conf/wm/hypr/scripts/monitors.sh
+++ conf/wm/hypr/scripts/monitors.sh
@@ -10,9 +10,9 @@ swww init
 sleep 0.1
 
 if [ "$(cat /etc/hostname)" == "arlo-nix" ]; then
-    # swww img -o DP-3 /etc/nixos/backgrounds/disco_elysium/cryptid.jpg
-    swww img -o HDMI-A-1 /etc/nixos/backgrounds/cp2077/DogTownFog.png
-    # swww img -o DP-2 /etc/nixos/backgrounds/disco_elysium/whirling_in_rags.jpg
+    swww img -o DP-1 /etc/nixos/backgrounds/disco_elysium/cryptid.jpg
+    # swww img -o HDMI-A-1 /etc/nixos/backgrounds/cp2077/DogTownFog.png
+    swww img -o DP-2 /etc/nixos/backgrounds/disco_elysium/whirling_in_rags.jpg
 else
     swww img -o eDP-1 /etc/nixos/backgrounds/rainbow-l.jpg
 fi
