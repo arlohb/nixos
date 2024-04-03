3aa63e0fd2f6c6d660943b8f111707c9a0b72ad1
arlohb <arlo@urwick.co.uk>
2024-04-03 15:19:46 +0100

    Switched ags config to full TS

--- conf/wm/ags/config.js
+++ conf/wm/ags/config.js
@@ -1,40 +1,18 @@
-const mpris = await Service.import("mpris");
+// From AGS wiki
+// https://aylur.github.io/ags-docs/config/type-checking/
 
-mpris.bind("players").as(players => {
-    const player = players[0];
-    console.log(player);
-});
+const entry = App.configDir + "/config.ts";
+const outdir = "/tmp/ags/js"
 
-mpris.connect("changed", (mpris, ...args) => {
-    print(mpris.getPlayer()?.cover_path);
-});
-
-const icon = Widget.Icon({
-    icon: mpris.bind("players").as(_ => mpris.getPlayer()?.cover_path ?? ""),
-    size: 100,
-});
-
-const label = Widget.Label({
-    label: "hello",
-});
-
-const bar = Widget.Window({
-    name: "bar",
-    anchor: ["left", "top", "bottom"],
-    exclusivity: "exclusive",
-    child: Widget.Box({
-        vertical: true,
-        children: [
-            icon,
-            label,
-        ],
-    }),
-});
-
-App.config({
-    windows: [bar],
-});
-
-// Make file a module for top level await
-export {}
+try {
+    await Utils.execAsync([
+        'bun', 'build', entry,
+        '--outdir', outdir,
+        '--external', 'resource://*',
+        '--external', 'gi://*',
+    ]);
+    await import(`file://${outdir}/config.js`);
+} catch (error) {
+    console.error(error);
+}
 
