97eddf04e12a6b0fb5b8423dde44e6a792c0f954
arlohb <arlo@urwick.co.uk>
2024-04-14 17:26:10 +0100

    AGS: Moved src and styles to separate folders and cleaned code generally

--- conf/wm/ags/config.js
+++ conf/wm/ags/config.js
@@ -1,10 +1,7 @@
 // From AGS wiki
 // https://aylur.github.io/ags-docs/config/type-checking/
 
-// TODO: Just move config.ts and music.ts to a src/ folder
-// TODO: Split up stylesheet? (and folderize)
-//       Maybe a stylesheet for each widget?
-const entry = App.configDir + "/config.ts";
+const entry = App.configDir + "/src/config.ts";
 const outdir = "/tmp/ags/js"
 
 try {
---
+++ conf/wm/ags/src/config.ts
@@ -0,0 +1,45 @@
+import music from "./music";
+
+/** Directory with all the css styles */
+const stylesPath = `${App.configDir}/styles`;
+
+/** The bar window */
+const bar = Widget.Window({
+    name: "bar",
+    anchor: ["left", "top", "bottom"],
+    exclusivity: "exclusive",
+    child: Widget.Box({
+        spacing: 8,
+        vertical: true,
+        className: "container",
+        children: [music(), music(), music()],
+    }),
+});
+
+/** Load the CSS files and return one css src text. */
+const loadCss = (): string => {
+    // Find files in styles/
+    return Utils.exec(`ls ${stylesPath}`)
+        .split("\n")
+        // Relative => absolute paths
+        .map(name => `${stylesPath}/${name}`)
+        // Read the files
+        .map(Utils.readFile)
+        // Join the files
+        .join("\n");
+};
+
+/** Apply the CSS to the app. */
+const applyCss = (): void => App.applyCss(loadCss(), true);
+
+// Create app
+App.config({ windows: [bar] });
+
+// Apply css
+applyCss();
+// Reload css when style changes
+Utils.monitorFile(stylesPath, applyCss);
+
+// Make file a module for top level await
+export {}
+
