edb1cfcb7872b4bb6f6fa769e441f32abdf2bb20
arlohb <arlo@urwick.co.uk>
2024-04-14 11:40:31 +0100

    AGS: Now checks for present MPRIS data

--- conf/wm/ags/config.ts
+++ conf/wm/ags/config.ts
@@ -1,3 +1,5 @@
+import Gtk from "types/@girs/gtk-3.0/gtk-3.0";
+
 const mpris = await Service.import("mpris");
 
 const music = () => Widget.Box({
@@ -6,25 +8,45 @@ const music = () => Widget.Box({
     className: "widget",
 }).hook(mpris, self => {
     const player = mpris.getPlayer();
-    const icon = Widget.Icon({
-        icon: player?.cover_path,
-        size: 100,
-    });
-    self.children = [
-        icon,
-        Widget.Label({
-            label: player?.track_title,
-            wrap: true,
-        }),
-        Widget.Label({
-            label: player?.track_album,
-            wrap: true,
-        }),
-        Widget.Label({
-            label: player?.track_artists.join(", "),
-            wrap: true,
-        }),
-    ];
+    const children: Gtk.Widget[] = [];
+
+    if (player?.cover_path && player?.cover_path !== "") {
+        children.push(
+            Widget.Icon({
+                icon: player?.cover_path,
+                size: 100,
+            })
+        );
+    }
+
+    if (player?.track_title && player?.track_title !== "") {
+        children.push(
+            Widget.Label({
+                label: player?.track_title,
+                wrap: true,
+            }),
+        );
+    }
+
+    if (player?.track_album && player?.track_album !== "") {
+        children.push(
+            Widget.Label({
+                label: player?.track_album,
+                wrap: true,
+            })
+        );
+    }
+
+    if (player?.track_artists && player?.track_artists.length !== 0) {
+        children.push(
+            Widget.Label({
+                label: player?.track_artists.join(", "),
+                wrap: true,
+            }),
+        );
+    }
+
+    self.children = children;
 });
 
 const bar = Widget.Window({
