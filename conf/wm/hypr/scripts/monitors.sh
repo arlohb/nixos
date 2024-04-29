806befa32634c7cf31babd68f87f1127a587d28f
arlohb <arlo@urwick.co.uk>
2024-04-29 11:37:50 +0100

    HYPR: Enabled all monitor wallpapers

--- conf/wm/hypr/scripts/monitors.sh
+++ conf/wm/hypr/scripts/monitors.sh
@@ -11,7 +11,7 @@ sleep 0.1
 
 if [ "$(cat /etc/hostname)" == "arlo-nix" ]; then
     swww img -o DP-1 /etc/nixos/backgrounds/cp2077/Pixel3.gif
-    # swww img -o HDMI-A-1 /etc/nixos/backgrounds/cp2077/DogTownFog.png
+    swww img -o HDMI-A-1 /etc/nixos/backgrounds/cp2077/Pixel3.gif
     swww img -o DP-2 /etc/nixos/backgrounds/cp2077/Pixel2.png
 else
     swww img -o eDP-1 /etc/nixos/backgrounds/rainbow-l.jpg
