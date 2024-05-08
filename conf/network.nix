b548b5edc3ea7acfd918943fa0d87678c4de2de8
arlohb <arlo@urwick.co.uk>
2024-05-08 16:40:21 +0100

    Added ssh aliases in secrets

--- conf/network.nix
+++ conf/network.nix
@@ -1,5 +1,8 @@
 { pkgs, ...}:
 
+let
+  secrets = import ../secrets.nix;
+in
 {
   # Enable mullvad vpn daemon
   services.mullvad-vpn.enable = true;
@@ -15,5 +18,8 @@
   persist.directories = [
     "/etc/mullvad-vpn"
   ];
+
+  # SSH aliases from secrets.nix
+  programs.ssh.extraConfig = secrets.sshExtraConfig;
 }
 
