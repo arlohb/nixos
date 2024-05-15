4fd4492d933ceaf080bf266b1b012d489e93e8dd
arlohb <arlo@urwick.co.uk>
2024-05-15 15:39:14 +0100

    AGS: Kill ags and run-ags before running

--- conf/ags/run-ags.sh
+++ conf/ags/run-ags.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Kill ags
+pkill --exact ags
+# Kill all run-ags except this process
+pgrep run-ags | grep -v $$ | xargs -r kill
+
 function run {
     ags --quit
     ags -c /etc/nixos/conf/ags/config.js &
