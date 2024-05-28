33649e5d91de186d7b5abdeb9149168639d76715
arlohb <arlo@urwick.co.uk>
2024-05-28 18:23:19 +0100

    TEMPLATES: Added build package to rust

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -8,6 +8,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
+
+        name = "name";
+        version = "0.0.1";
+        deps = with pkgs; [];
+
+        package = pkgs.rustPlatform.buildRustPackage {
+          inherit version;
+          pname = name;
+
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = deps;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -16,7 +29,12 @@
             rust-analyzer
             clippy
             rustfmt
-          ];
+          ] ++ deps;
+        };
+
+        packages = rec {
+          "${name}" = package;
+          default = package;
         };
       }
     );
