5dac5175fad9e480d8aef4a23f5f3e640781b46a
arlohb <arlo@urwick.co.uk>
2024-07-01 19:39:33 +0100

    AGS: Added mute indicator

--- conf/ags/src/widgets/slider/index.ts
+++ conf/ags/src/widgets/slider/index.ts
@@ -1,11 +1,12 @@
 import type { SliderProps } from "types/widgets/slider";
 import brightnessService from "@services/brightness";
+import type { Binding } from "types/service";
 
 const { speaker } = await Service.import("audio");
 
 const slider = (
-    icon: string,
-    value: SliderProps["value"],
+    icon: string | Binding<any, any, string>,
+    value: number | Binding<any, any, number>,
     onChange: SliderProps["onChange"],
 ) => (
     orientation: "H" | "V",
@@ -13,7 +14,9 @@ const slider = (
     className: `widget slider slider${orientation}`,
     orientation : orientation == "H" ? 0 : 1,
     children: [
-        Widget.Label(icon),
+        Widget.Label({
+            label: icon,
+        }),
         Widget.Slider({
             orientation : orientation == "H" ? 0 : 1,
             drawValue: false,
@@ -25,8 +28,14 @@ const slider = (
 
 export default slider;
 
+// speaker.stream is used for real is_muted, but isn't changed when needed
+// So whenever speaker is changed, trigger a change on stream
+speaker.connect("changed", _speaker => {
+    speaker.notify("stream");
+});
+
 export const volume = slider(
-    "󰕾",
+    speaker.bind("stream").as(stream => stream?.is_muted ? "󰝟" : "󰕾"),
     speaker.bind("volume"),
     ({ value }) => speaker.volume = value,
 );
