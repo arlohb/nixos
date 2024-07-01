98f5a1a0750a9d74214c28f3be1e56fc0212528c
arlohb <arlo@urwick.co.uk>
2024-07-01 19:09:11 +0100

    Added mute key

--- conf/wm/hypr/scripts/volume.fish
+++ conf/wm/hypr/scripts/volume.fish
@@ -17,6 +17,7 @@ if test (count $argv) -eq 0
     echo -e "  get"
     echo -e "  inc [+/-] [step]"
     echo -e "  set [vol] [step]"
+    echo -e "  togglemute"
     echo -e ""
     exit
 end
@@ -36,6 +37,8 @@ case inc
     # Calculate new volume
     set vol $(math "$vol $op $step")
 
+    # Unmute
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
     # Set new volume
     wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ {$vol}%
 
@@ -47,9 +50,24 @@ case set
     # Floor to multiple of step
     set vol $(math "$vol - ($vol % $step)")
 
+    # Unmute
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
     # Set new volume
     wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ {$vol}%
 
     notify $vol
+case togglemute
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+
+    set is_muted $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f3)
+
+    # Is string length non-zero
+    if test -n $is_muted
+        echo "Is muted"
+        notify 0
+    else
+        echo "Not muted"
+        notify $(get_vol)
+    end
 end
 
