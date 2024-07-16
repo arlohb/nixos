09d8643acb056d7e7cb6fb1adaef0f8aba2ace29
arlohb <arlo@urwick.co.uk>
2024-07-16 18:05:23 +0100

    Update

--- conf/3d.nix
+++ conf/3d.nix
@@ -3,7 +3,10 @@
 {
   pkgs = with pkgs; [
     blender
-    cura
+    # TODO: Add cura to nix-flatpak and remove note here
+    # https://github.com/NixOS/nixpkgs/issues/186570
+    # Best to use flatpak until this is fixed
+    # cura
     openscad
   ];
 
