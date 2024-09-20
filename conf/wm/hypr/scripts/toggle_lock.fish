8cb5d9ae5aabee383ed43b49cd68a68c3bf133a7
arlohb <arlo@urwick.co.uk>
2024-09-20 22:11:41 +0100

    Fixed arlo-nix monitor refresh rate

--- conf/wm/hypr/scripts/toggle_lock.fish
+++ conf/wm/hypr/scripts/toggle_lock.fish
@@ -5,12 +5,12 @@ set main_x (hyprctl monitors -j | jq ".[] | select([.width == 2560] | any) | .x"
 if [ $main_x = 1920 ]
     dunstify "Enabling mouse lock"
 
-    hyprctl keyword monitor HDMI-A-1, preferred, 10000x0, 1
+    hyprctl keyword monitor HDMI-A-1, 2560x1080@75, 10000x0, 1
     hyprctl keyword monitor DP-2, preferred, 20000x0, 1, transform, 1
 else
     dunstify "Disabling mouse lock"
 
-    hyprctl keyword monitor HDMI-A-1, preferred, 1920x0, 1
+    hyprctl keyword monitor HDMI-A-1, 2560x1080@75, 1920x0, 1
     hyprctl keyword monitor DP-2, preferred, (math 1920+2560)x0, 1, transform, 1
 end
 
