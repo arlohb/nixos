d9e786a2afc52a75eb11c6e9a77f8872392244e5
arlohb <arlo@urwick.co.uk>
2024-12-29 12:40:52 +0000

    Made arlo-laptop1 headless

--- hosts/arlo-laptop1.nix
+++ hosts/arlo-laptop1.nix
@@ -5,26 +5,14 @@
   ../conf/hardware.nix
   ../conf/user.nix
   ../conf/shell.nix
-  ../conf/audio.nix
-
-  # Desktop
-  ../conf/wm/core.nix
-  ../conf/wm/cursor.nix
-  ../conf/wm/login.nix
-  ../conf/wm/notifications.nix
-  ../conf/wm/polkit.nix
-  ../conf/wm/hypr/hypr.nix
 
   # Programs
   ../conf/git.nix
   ../conf/neovim.nix
   ../conf/network.nix
-  ../conf/programs.nix
-  ../conf/terminal.nix
 
   # Server
   ../conf/server/core.nix
-  ../conf/server/cloudflare-dns.nix
   ../conf/server/docker.nix
   ../conf/server/duckdns.nix
   ../conf/server/firewall.nix
