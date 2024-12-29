71abf6fd82c5eb35fb11ddcf7da11add51b18a46
arlohb <arlo@urwick.co.uk>
2024-12-29 12:44:20 +0000

    Add comments to rebuild-firewall.sh

--- conf/server/rebuild-firewall.sh
+++ conf/server/rebuild-firewall.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# Flush the rules
 iptables -F
+
+# Load the new rules service
 nixos-rebuild switch
+
+# Flush the rules
 iptables -F
+
+# Apply the rules
 systemctl restart firewall
 systemctl restart docker
 systemctl restart firewall-rules
