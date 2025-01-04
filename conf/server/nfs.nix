8a8f5eb645b30a7021ae089355b59c4ffebb2ce9
arlohb <arlo@urwick.co.uk>
2025-01-04 23:17:33 +0000

    Created interface over nfs shares

--- conf/server/nfs.nix
+++ conf/server/nfs.nix
@@ -1,18 +1,47 @@
 { pkgs, ... }:
-
-{
-  fileSystems."/export/volumes/jellyseerr" = {
-    device = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/jellyseerr";
-    options = [ "bind" ];
-  };
+let
+  shares = builtins.map
+    (share: {
+      allowRoot = false;
+    } // share)
+    [
+      {
+        exportPath = "/export/volumes/jellyseerr";
+        srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/jellyseerr";
+        IPs = [ "10.0.0.220" ];
+      }
+      {
+        exportPath = "/export/volumes/nodered";
+        srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/nodered";
+        IPs = [ "10.0.0.220" ];
+      }
+    ];
+in {
+  fileSystems = builtins.listToAttrs (
+    builtins.map
+      (share: {
+        name = share.exportPath;
+        value = {
+          device = share.srcPath;
+          options = [ "bind" ];
+        };
+      })
+      shares
+  );
 
   services.nfs.server = {
     enable = true;
-    # no_root_squash means root can access the files on the client
-    # Useful when debugging, but not needed with docker compose
-    exports = ''
-      /export/volumes/jellyseerr  10.0.0.220(rw,sync)
-    '';
+
+    exports = pkgs.lib.strings.concatMapStrings
+      (share: let
+        rootSquashOption = if share.allowRoot then ",no_root_squash" else "";
+        hosts = pkgs.lib.strings.concatMapStrings
+          (ip: " ${ip}(rw,sync${rootSquashOption})")
+          share.IPs;
+      in
+        "${share.exportPath}${hosts}\n"
+      )
+      shares;
   };
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
