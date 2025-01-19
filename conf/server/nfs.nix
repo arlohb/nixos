a1cab04070aa33705820f555751a136989211b90
arlohb <arlo@urwick.co.uk>
2025-01-19 14:42:59 +0000

    Switched NFS share IPs to CIDR notation

--- conf/server/nfs.nix
+++ conf/server/nfs.nix
@@ -2,6 +2,7 @@
 let
   shares = builtins.map
     (share: {
+      IPs = [ "10.0.0.0/24" ];
       allowRoot = false;
     } // share)
     [
@@ -10,53 +11,43 @@ let
       {
         exportPath = "/export/volumes/nodered";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/nodered";
-        IPs = [ "10.0.0.220" ];
       }
       {
         exportPath = "/export/volumes/wireguard";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/wireguard";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/qbt/config";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/qbt/config";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/qbt/downloads";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/qbt/downloads";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/prowlarr";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/prowlarr";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/radarr";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/radarr";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/sonarr";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/sonarr";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/readarr";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/readarr";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/volumes/jellyseerr";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/jellyseerr";
-        IPs = [ "10.0.0.154" ];
         allowRoot = true;
       }
       {
         exportPath = "/export/volumes/kiwix";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Volumes/kiwix";
-        IPs = [ "10.0.0.220" ];
       }
 
       # Media
@@ -64,22 +55,18 @@ let
       {
         exportPath = "/export/media/movies";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Media/Movies";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/media/shows";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Media/Shows";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/media/books/audiobooks";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Media/Books/Audiobooks";
-        IPs = [ "10.0.0.154" ];
       }
       {
         exportPath = "/export/media/books/books";
         srcPath = "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data/_data/admin/files/Media/Books/Books";
-        IPs = [ "10.0.0.154" ];
       }
     ];
 in {
