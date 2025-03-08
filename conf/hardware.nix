5c00d2d6b4a3b56572ef7e95556b2b58864d9f89
arlohb <arlo@urwick.co.uk>
2025-03-08 14:14:52 +0000

    Removed backup drive from arlo-laptop1

--- conf/hardware.nix
+++ conf/hardware.nix
@@ -81,16 +81,6 @@
       device = "/dev/disk/by-label/steam";
       fsType = "ext4";
     };
-  } else if hostname == "arlo-laptop1" then {
-    "/mnt" = {
-      device = "/dev/disk/by-label/backup";
-      fsType = "ext4";
-    };
-
-    "/home/arlo/code/vps/nextcloud_backups" = {
-      device = "/mnt/nextcloud_backups";
-      options = [ "bind" ];
-    };
   } else { });
 } // (if hostname == "arlo-nix" then {
   # Only useful on pc, because RAM there only fills up,
