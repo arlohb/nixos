21e4c8ce2c2a16c3831314ec23e929bbc9ccb928
arlohb <arlo@urwick.co.uk>
2025-03-10 08:49:33 +0000

    Removed sofle from kmonad

--- conf/wm/kbd/kmonad.nix
+++ conf/wm/kbd/kmonad.nix
@@ -1,25 +1,21 @@
 { pkgs, ... }:
-let
-  options = {
-    defcfg = {
-      enable = true;
-      # Idk if I always want this,
-      # but I figure it helps me not lock myself out
-      fallthrough = true;
-      # Don't use, unnecessary attack surface
-      allowCommands = false;
-    };
 
-    config = builtins.readFile ./arlo-laptop2.kbd;
-  };
-in {
+{
   services.kmonad = {
     enable = true;
-    keyboards.laptopKbd = options // {
+    keyboards.laptopKbd = {
       device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-    };
-    keyboards.sofleKbd = options // {
-      device = "/dev/input/by-id/usb-JosefAdamcik_Sofle_E463A8574B543B2D0000000000000000-event-kbd";
+
+      defcfg = {
+        enable = true;
+        # Idk if I always want this,
+        # but I figure it helps me not lock myself out
+        fallthrough = true;
+        # Don't use, unnecessary attack surface
+        allowCommands = false;
+      };
+
+      config = builtins.readFile ./arlo-laptop2.kbd;
     };
   };
 }
