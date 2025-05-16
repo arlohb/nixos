f849e73e223eab83fc2929c32073973410d29b06
arlohb <arlo@urwick.co.uk>
2025-05-16 11:18:03 +0100

    Added cpp23 template

---
+++ templates/cpp23/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A c++ 23 project using CMake, clang, ccls";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ccls
+            clang_16
+            cmake
+            gdb
+          ];
+        };
+      }
+    );
+}
---
+++ templates/cpp23/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p build
+cd build
+cmake ..
+make
+./newProject
+
