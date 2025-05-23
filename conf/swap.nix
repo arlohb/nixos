3ff3799cda714ac2b43a7d478efaa232c95e3428
arlohb <arlo@urwick.co.uk>
2025-05-23 13:11:23 +0100

    Reduced swap on arlo-laptop2

--- conf/swap.nix
+++ conf/swap.nix
@@ -1,10 +1,12 @@
-{ ... }:
+{ hostname, ... }:
 
 {
   swapDevices = [
     {
       device = "/nix/var/swap";
-      size = 16*1024; # In MB, 16 GB
+      size = if hostname == "arlo-laptop2"
+        then 8*1024  # In MB, 64 GB
+        else 16*1024; # In MB, 16 GB
     }
   ];
 }
