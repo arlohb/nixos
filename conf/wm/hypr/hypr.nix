410cdf1ab4d1c83835aa66ecd9d8391ce2b19acb
arlohb <arlo@urwick.co.uk>
2025-06-13 00:25:00 +0100

    Remove watch_monitors

--- conf/wm/hypr/hypr.nix
+++ conf/wm/hypr/hypr.nix
@@ -6,8 +6,6 @@
   pkgs = with pkgs; [
     # Used to disable xorg / xwayland access control
     xorg.xhost
-    # Used in watch_monitors.sh
-    socat
   ];
 
   # This value is 'compile-time' checked somehow
